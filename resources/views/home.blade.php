cf667ba20473550525dea7e619acf120e3a9a7b3
Zhahiraadriyani <151808968+Zhahiraadriyani@users.noreply.github.com>
2023-12-04 12:12:22 +0700

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+@section('judul')
+Aplikasi Kasir | Zhahira Adriyani Pocket
+@endsection
+@section('judul_sub')
+Halaman Utama Sesudah Login
+@endsection
+@section('content')
+<div class="h2 mb-3 text-center">Halaman Utama</div>
+<hr style="width:75%">
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-warning">Aplikasi Kasir Sederhana</h6>
+    </div>
+    <div class="card-body">
+        <div class="h2">Project 1 - Aplikasi Kasir Redesign by Zhahira Adriyani</div>
+        <hr style="width:50%">
+        <div class="h4"> Fitur dalam aplikasi kasir sederhana yang mencakup: </div>
+        <ul>
+            <li>Mencatat transaksi pembelian barang</li>
+            <li>Menampilkan daftar transaksi pembelian barang</li>
+            <li>Authentikasi login pengguna</li>
+            <li>Pengaturan data pengguna</li>
+            <li>Pengaturan data master produk</li>
+        </ul>
+    </div>
+</div>
+@endsection
